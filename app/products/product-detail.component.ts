51418ea5cddef2e2f9d2b38046fe0490672b8beb
Geoff Schultz <schultz.robert.g@gmail.com>
2016-12-24 17:06:07 -0800

    Add ProductDetail view

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
 @Component({
-    //moduleId: module.id,
     templateUrl: 'app/products/product-detail.component.html'
 })
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
     product: IProduct;
 
-    ngOnInit(): void{}
+    constructor(private _route: ActivatedRoute){
+    }
+
+    ngOnInit(): void{
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 
 }
